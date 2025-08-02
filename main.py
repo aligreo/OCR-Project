bc0dee383d6b60f26c0206e5daf1eecd0fee1f0d
aligreo <agreu77@yahoo.com>
2025-08-02 21:11:44 +0300

    .

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("hello world")
-print("this is a test file")
+
+from transformers import pipeline
+
+ocr = pipeline(task="image-to-text", model="microsoft/trocr-base-handwritten")
+
+ocr(r"C:\Users\agreu\OCR-Project\images\Copy-of-Blog-Image-Template.png")
+
